bc0cbf28945d257bb6e12bcf2e931057e87cfb2b
EduardoTeles12 <166310629+EduardoTeles12@users.noreply.github.com>
2025-06-26 17:36:26 +0000

    Remover textos do topo das telas de login e cadastro

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -3,7 +3,6 @@ import "./logo.css";
 export const Logo = () => {
   return (
     <div className="logo">
-      <div className="text-wrapper">Acessar conta</div>
     </div>
   );
 };
--- src/components/Logo2/index.jsx
+++ src/components/Logo2/index.jsx
@@ -3,7 +3,6 @@ import "./logo2.css";
 export const Logo2 = () => {
   return (
     <div className="logo">
-      <div className="text-wrapper">Cadastre-se</div>
     </div>
   );
 };
